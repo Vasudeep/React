01671c2beff5bd5fe0843844c0c911d9085edf89
Vasudha Singh <86353004+Vasudeep@users.noreply.github.com>
2021-10-29 23:28:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+        <div className="section1">
+          <h3><hr></hr>Your Customized Online Banking Partner</h3>
+          <h4>Plan your financial future. Compare, analyse & apply for various banking, credit
+            and investment solutions based on your specific requirements.
+
+          </h4>
+          <hr></hr>
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
